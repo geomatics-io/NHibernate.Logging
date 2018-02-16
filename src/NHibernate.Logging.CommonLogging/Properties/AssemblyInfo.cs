e638c2f9643fda04daebe53917a31ddac848e6be
Christian Junk <christian.junk@amecfw.com>
2018-02-16 18:16:57 +0100

    Updated AssemblyInfo.

--- src/NHibernate.Logging.CommonLogging/Properties/AssemblyInfo.cs
+++ src/NHibernate.Logging.CommonLogging/Properties/AssemblyInfo.cs
@@ -3,17 +3,17 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("NHibernate.Logging.CommonLogging")]
-[assembly: AssemblyDescription("Common.Logging 3.1.x Logging Provider for NHibernate 4.0.x")]
+[assembly: AssemblyDescription("Common.Logging 3.x.x Logging Provider for NHibernate 4.x.x")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NHibernate.Logging.CommonLogging")]
-[assembly: AssemblyCopyright("Copyright © Matthias Gernand 2011-2015")]
+[assembly: AssemblyCopyright("©Copyright Matthias Gernand 2011-2015, ©Copyright 2017-2018 geomatics.io")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: Guid("9793cd5f-e84f-4e24-b21c-32221b7913be")]
-[assembly: AssemblyVersion("4.0.3.4000")]
-[assembly: AssemblyFileVersion("4.0.3.4000")]
+[assembly: AssemblyVersion("4.1.1.4000")]
+[assembly: AssemblyFileVersion("4.1.1.4000")]
 [assembly: AssemblyInformationalVersion("4.0.3.4000")]
