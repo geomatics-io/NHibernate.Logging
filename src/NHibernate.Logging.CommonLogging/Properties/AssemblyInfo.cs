cf29cf6d5b2d109d3acc7bac7b19bf3b5e2658f4
Christian Junk <christian.junk@woodplc.com>
2019-07-10 11:35:26 +0200

    Updated to .NEt 4.6.2 and NHibernate 5.2.1.

--- src/NHibernate.Logging.CommonLogging/Properties/AssemblyInfo.cs
+++ src/NHibernate.Logging.CommonLogging/Properties/AssemblyInfo.cs
@@ -3,17 +3,17 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("NHibernate.Logging.CommonLogging")]
-[assembly: AssemblyDescription("Common.Logging 3.x Logging Provider for NHibernate 4.x")]
+[assembly: AssemblyDescription("Common.Logging 3.x Logging Provider for NHibernate 5.x")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NHibernate.Logging.CommonLogging")]
-[assembly: AssemblyCopyright("©Copyright Matthias Gernand 2011-2015, ©Copyright 2017-2018 geomatics.io")]
+[assembly: AssemblyCopyright("©Copyright Matthias Gernand 2011-2015, ©Copyright 2017-2019 geomatics.io")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: Guid("9793cd5f-e84f-4e24-b21c-32221b7913be")]
-[assembly: AssemblyVersion("4.1.1.4000")]
-[assembly: AssemblyFileVersion("4.1.1.4000")]
-[assembly: AssemblyInformationalVersion("4.1.1.4000")]
+[assembly: AssemblyVersion("5.2.1")]
+[assembly: AssemblyFileVersion("5.2.1")]
+[assembly: AssemblyInformationalVersion("5.2.1")]
